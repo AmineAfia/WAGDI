0aec0685f785a1a39b962b2bcb8382d763506acf
Amine Afia <afia.amine@gmail.com>
2022-09-17 18:15:13 +0200

    dummy image

--- pages/reveal.tsx
+++ pages/reveal.tsx
@@ -1,10 +1,16 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Image, Heading } from "@chakra-ui/react";
 import Link from "next/link";
 
 const Reveal = () => {
 	return (
 		<Container paddingY="10">
 			<Heading>Reveal Page</Heading>
+			<Image
+				borderRadius="full"
+				boxSize="150px"
+				src="https://bit.ly/dan-abramov"
+				alt="Dan Abramov"
+			/>
 			<Link href="/result"> result </Link>
 		</Container>
 	);
